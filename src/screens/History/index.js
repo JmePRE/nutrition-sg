1304f0f24976be0e61215ff3be6cffbc6eba0b84
Jaime PAT <jaime.pang.rei.ern@gmail.com>
2022-02-27 11:25:03 +0800

    Add tables

--- src/screens/History/index.js
+++ src/screens/History/index.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react'
+import { Table, Button } from 'antd'
 
 function HistoryScreen(props) {
-  /*
-  const {
-    match: { url },
-  } = props
-  */
+  const userHistory = [
+    {
+      name: 'Brown rice laksa noodles, cooked',
+      energy: '107 kcal',
+      time_added: 1645932161,
+    },
+    {
+      name: 'Brown rice laksa noodles, cooked',
+      energy: '107 kcal',
+      time_added: 1645932161,
+    },
+    {
+      name: 'Brown rice laksa noodles, cooked',
+      energy: '107 kcal',
+      time_added: 1645932161,
+    },
+  ]
 
-  return <div>hello</div>
+  return (
+    <div>
+      <h1>My Food Log</h1>
+      <Table
+        title={() => <h3>Deal Information</h3>}
+        columns={[
+          {
+            title: 'Name of Food',
+            dataIndex: 'name',
+            key: 'energy',
+          },
+          {
+            title: 'Energy (Kcal)',
+            dataIndex: 'energy',
+            key: 'energy',
+          },
+          {
+            title: 'Time Added',
+            dataIndex: 'time_added',
+            key: 'time_added',
+            render: (text, record) => (
+              <div className="dates">
+                <p>{new Date(record.time_added * 1000)}</p>
+              </div>
+            ),
+          },
+          {
+            title: 'Add',
+            dataIndex: 'add',
+            key: 'add',
+            render: () => <Button>Add to Log</Button>,
+          },
+        ]}
+        dataSource={userHistory}
+        pagination={false}
+      />
+    </div>
+  )
 }
 
 export default HistoryScreen
